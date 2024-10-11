7c670716e03b53f8b97416e787832c4cfe315207
Fellps-collab <jjonasffelipefogo@gmail.com>
2024-10-11 18:31:38 -0300

    Upload de aplicação de  pesquisa de altetas

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a fadinha, é uma skatista brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
